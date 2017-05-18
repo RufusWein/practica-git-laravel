10543862524ae817084a8a1ed9fc7f56f5671589
Pedro <sr.whiteskull@gmail.com>
2017-05-18 15:17:31 +0000

    segunda actulizacion

---
+++ resources/views/nuevaVista.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Hola, soy Pedro, y esto es un ejercicio Github para Entornos de Desarrollo<br>";
+echo "<br><br>";
+echo "Para subirlo primero creo el repositorio en <strong>Github</strong><br>";
+echo ", y luego ejecuto los comandos indicados en la página.<br>";
+
+?>
